f9b991dee0b6a365fd4cbdba4e110f07f15aa8e5
Chaxxklim <87353327+Chaxxklim@users.noreply.github.com>
2021-11-23 20:50:40 +0900

    칵하오

--- src/FailRate.java
+++ src/FailRate.java
@@ -1,7 +1,30 @@
-class FailRate {
-    public int[] solution(int N, int[] stages) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+class FailRate {
+    public static int[] solution(int N, int[] stages) {
         int[] answer = {};
+        int[] tempArray = new int[stages.length];
+        int[][] temp = new int[N+1][1];
+        Arrays.sort(stages); // 정렬
+        for (int i = 0; i < stages.length; i++) {
+            int num = 0;
+            if (num == stages[i]){
+                num++;
+            }
+            tempArray[i] = num;
+        }
+        System.out.println(Arrays.toString(tempArray));
+        System.out.println(Arrays.deepToString(temp));
+        System.out.println(Arrays.toString(stages));
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int N = 5;
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        solution(N, stages);
     }
 }
